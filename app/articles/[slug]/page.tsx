a989bcd74906abd3f9f0874af91624d4d3cbc29f
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-05-16 22:02:05 +0900

    add: 詳細画面の作り込み

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -2,6 +2,12 @@ import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { Article, Comment } from "@/app/types";
 import type { Metadata, ResolvingMetadata } from 'next';
+import ArticleContent from "./ArticleContent";
+import Comments from "./Comments";
+import { Heading } from "@/app/common/components";
+import LoadingComments from "./LoadingComments";
+
+
 
 export async function generateMetadata({
     params,
@@ -64,28 +70,29 @@ export default async function ArticleDetail({
 
     return (
         <div>
-            <h1>{article.title}</h1>
-            <p>{article.content}</p>
-            <h2>Comments</h2>
+            <ArticleContent article={article} />
+            <Heading as="h2" mt={8} mb={4}>
+                Comments
+            </Heading>
             {/* @ts-expect-error 現状は jsx が Promise を返すと TypeScript が型エラーを報告するが、将来的には解決される */}
-            <Suspense fallback={<div>Loading comments...</div>}>
+            <Suspense fallback={<LoadingComments />}>
                 <Comments commentPromise={commentsPromise} />
             </Suspense>
         </div>
     )
 }
 
-async function Comments({
-    commentPromise,
-}: {
-    commentPromise: Promise<Comment[]>;
-}) {
-    const comments = await commentPromise;
-    return (
-        <ul>
-            {comments?.map((comment) => (
-                <li key={comment.id}>{comment.body}</li>
-            ))}
-        </ul>
-    )
-}
+// async function Comments({
+//     commentPromise,
+// }: {
+//     commentPromise: Promise<Comment[]>;
+// }) {
+//     const comments = await commentPromise;
+//     return (
+//         <ul>
+//             {comments?.map((comment) => (
+//                 <li key={comment.id}>{comment.body}</li>
+//             ))}
+//         </ul>
+//     )
+// }
