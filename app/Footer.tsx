80750e0014974bb170c5a56eb67b3cfd373de39e
Ryo <matsukawa.ryo42@gmail.com>
2023-05-17 22:56:11 +0900

    add: footerを中央に寄せた

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,10 +1,12 @@
-import { Container, Box, Text } from './common/components/';
+import { Container, Box, Text, Center } from './common/components/';
 
 export default function Footer() {
   return (
     <Box bg='gray.50' color='gray.700' as='footer'>
       <Container maxW='5xl' py={4}>
-        <Text as='small'>© 2023 ryo-manba</Text>
+        <Center>
+          <Text as='small'>© 2023 ryo-manba</Text>
+        </Center>
       </Container>
     </Box>
   );
